624f08b895d3a0a09eb2b9c4a7e789c533bd9522
Jess Archer <jess@jessarcher.com>
2022-12-06 15:44:08 +1000

    Capture hits per user

--- src/Handlers/HandleHttpRequest.php
+++ src/Handlers/HandleHttpRequest.php
@@ -4,8 +4,9 @@ namespace Laravel\Pulse\Handlers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Routing\RouteAction;
+use Illuminate\Support\Facades\Redis;
+use Symfony\Component\HttpFoundation\Response;
 
 class HandleHttpRequest
 {
@@ -14,6 +15,12 @@ class HandleHttpRequest
      */
     public function __invoke(Carbon $startedAt, Request $request, Response $response): void
     {
+        if ($request->user()) {
+            $key = 'hits:' . $startedAt->format('Ymd');
+            Redis::zIncrBy($key, 1, $request->user()->id);
+            Redis::expireAt($key, $startedAt->toImmutable()->startOfDay()->addDays(7)->timestamp);
+        }
+
         ray('Request Duration: '.$startedAt->diffInMilliseconds(now()).'ms');
 
         $action = $request->route()?->getAction();
