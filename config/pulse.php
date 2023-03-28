c367f3e324f6b870ef92a66d7d5224dec87e1eb0
Jess Archer <jess@jessarcher.com>
2023-03-28 15:05:37 +1000

    Update default slow endpoint threshold

--- config/pulse.php
+++ config/pulse.php
@@ -13,7 +13,7 @@ return [
     'server_name' => env('PULSE_SERVER_NAME', gethostname()),
 
     // in milliseconds
-    'slow_endpoint_threshold' => 3000,
+    'slow_endpoint_threshold' => 1000,
 
     // in milliseconds
     'slow_query_threshold' => 1000,
