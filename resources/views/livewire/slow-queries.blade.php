434688e7c28d7bb36905ccd61aebf241edc605af
Tim MacDonald <hello@timacdonald.me>
2023-06-16 15:43:04 +1000

    wip slow queries component

--- resources/views/livewire/slow-queries.blade.php
+++ resources/views/livewire/slow-queries.blade.php
@@ -5,54 +5,84 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             <span>
-                Slow Queries
-                <small class="ml-2 text-gray-400 text-xs font-medium">&gt;&equals;{{ config('pulse.slow_query_threshold') }}ms, past 7 days</small>
+                <span title="Time: {{ $time }}ms; Run at: {{ $runAt }}">Slow Queries</span>
+                <small class="ml-2 text-gray-400 text-xs font-medium">past {{ match ($this->period) {
+                    '6_hours' => '6 hours',
+                    '24_hours' => '24 hours',
+                    '7_days' => '7 days',
+                    default => 'hour',
+                } }}, &gt;&equals;{{ config('pulse.slow_query_threshold') }}ms</small>
             </span>
         </x-pulse::card-title>
     </x-slot:title>
 
-    @if (count($slowQueries) === 0)
-        <x-pulse::no-results />
-    @else
-        <div class="max-h-56 h-full relative overflow-y-auto">
-            <x-pulse::table class="table-fixed">
-                <x-pulse::thead>
-                    <tr>
-                        <x-pulse::th class="text-left">Query</x-pulse::th>
-                        <x-pulse::th class="text-right w-24">Count</x-pulse::th>
-                        <x-pulse::th class="text-right w-24">Average</x-pulse::th>
-                        <x-pulse::th class="text-right w-24">Slowest</x-pulse::th>
-                    </tr>
-                </x-pulse::thead>
-                <tbody>
-                    @foreach ($slowQueries as $query)
-                        <tr>
-                            <x-pulse::td>
-                                <code class="block text-xs text-gray-900 truncate" title="{{ $query['sql'] }}">
-                                    {{ $query['sql'] }}
-                                </code>
-                            </x-pulse::td>
-                            <x-pulse::td class="text-right text-gray-700 text-sm w-24">
-                                <strong>{{ $query['execution_count'] }}</strong>
-                            </x-pulse::td>
-                            <x-pulse::td class="text-right text-gray-700 text-sm w-24 whitespace-nowrap">
-                                @if ($query['average_duration'] === null)
-                                    <strong>Unknown</strong>
-                                @else
-                                    <strong>{{ $query['average_duration'] ?: '<1' }}</strong> ms
-                                @endif
-                            </x-pulse::td>
-                            <x-pulse::td class="text-right text-gray-700 text-sm w-24 whitespace-nowrap">
-                                @if ($query['slowest_duration'] === null)
-                                    <strong>Unknown</strong>
-                                @else
-                                    <strong>{{ $query['slowest_duration'] ?: '<1' }}</strong> ms
-                                @endif
-                            </x-pulse::td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </x-pulse::table>
+    <div class="max-h-56 h-full relative overflow-y-auto" wire:poll.5s>
+        <script>
+            const initialSlowQueriesDataLoaded = @js($initialDataLoaded)
+        </script>
+        <div x-data="{
+            initialDataLoaded: initialSlowQueriesDataLoaded,
+            loadingNewDataset: false,
+            init() {
+                Livewire.on('periodChanged', () => (this.loadingNewDataset = true))
+
+                window.addEventListener('slow-queries:dataLoaded', () => {
+                    this.initialDataLoaded = true
+                    this.loadingNewDataset = false
+                })
+
+                if (! this.initialDataLoaded) {
+                    @this.loadData()
+                }
+            }
+        }">
+            <div x-cloak x-show="! initialDataLoaded">Loading...</div>
+            <div x-cloak x-show="initialDataLoaded">
+                <div :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
+                    @if ($initialDataLoaded && count($slowQueries) === 0)
+                        <x-pulse::no-results />
+                    @elseif ($initialDataLoaded && count($slowQueries) > 0)
+                        <x-pulse::table class="table-fixed">
+                            <x-pulse::thead>
+                                <tr>
+                                    <x-pulse::th class="text-left">Query</x-pulse::th>
+                                    <x-pulse::th class="text-right w-24">Count</x-pulse::th>
+                                    <x-pulse::th class="text-right w-24">Average</x-pulse::th>
+                                    <x-pulse::th class="text-right w-24">Slowest</x-pulse::th>
+                                </tr>
+                            </x-pulse::thead>
+                            <tbody>
+                                @foreach ($slowQueries as $query)
+                                    <tr>
+                                        <x-pulse::td>
+                                            <code class="block text-xs text-gray-900 truncate" title="{{ $query['sql'] }}">
+                                                {{ $query['sql'] }}
+                                            </code>
+                                        </x-pulse::td>
+                                        <x-pulse::td class="text-right text-gray-700 text-sm w-24">
+                                            <strong>{{ $query['execution_count'] }}</strong>
+                                        </x-pulse::td>
+                                        <x-pulse::td class="text-right text-gray-700 text-sm w-24 whitespace-nowrap">
+                                            @if ($query['average_duration'] === null)
+                                                <strong>Unknown</strong>
+                                            @else
+                                                <strong>{{ $query['average_duration'] ?: '<1' }}</strong> ms
+                                            @endif
+                                        </x-pulse::td>
+                                        <x-pulse::td class="text-right text-gray-700 text-sm w-24 whitespace-nowrap">
+                                            @if ($query['slowest_duration'] === null)
+                                                <strong>Unknown</strong>
+                                            @else
+                                                <strong>{{ $query['slowest_duration'] ?: '<1' }}</strong> ms
+                                            @endif
+                                        </x-pulse::td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </x-pulse::table>
+                    @endif
+                </div>
+            </div>
         </div>
-    @endif
+    </div>
 </x-pulse::card>
--- src/Http/Livewire/SlowQueries.php
+++ src/Http/Livewire/SlowQueries.php
@@ -2,16 +2,97 @@
 
 namespace Laravel\Pulse\Http\Livewire;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 use Laravel\Pulse\Contracts\ShouldNotReportUsage;
+use Laravel\Pulse\Http\Livewire\Concerns\HasPeriod;
 use Laravel\Pulse\Pulse;
 use Livewire\Component;
 
 class SlowQueries extends Component implements ShouldNotReportUsage
 {
+    use HasPeriod;
+
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
     public function render(Pulse $pulse)
     {
+        if (request()->hasHeader('X-Livewire')) {
+            $this->loadData();
+        }
+
+        [$slowQueries, $time, $runAt] = $this->slowQueries();
+
         return view('pulse::livewire.slow-queries', [
-            'slowQueries' => $pulse->slowQueries(),
+            'time' => $time,
+            'runAt' => $runAt,
+            'slowQueries' => $slowQueries,
+            'initialDataLoaded' => $slowQueries !== null,
         ]);
+    }
+
+    /**
+     * The slow queries.
+     *
+     * @return array
+     */
+    protected function slowQueries()
+    {
+        return Cache::get("pulse:slow-queries:{$this->period}") ?? [null, 0, null];
+    }
+
+    /**
+     * Load the data for the component.
+     *
+     * @return void
+     */
+    public function loadData()
+    {
+        Cache::remember("pulse:slow-queries:{$this->period}", now()->addSeconds(match ($this->period) {
+            '6_hours' => 30,
+            '24_hours' => 60,
+            '7_days' => 600,
+            default => 5,
+        }), function () {
+            $now = now()->toImmutable();
+
+            $start = hrtime(true);
+
+            $slowQueries = DB::table('pulse_requests')
+                ->selectRaw('route, COUNT(*) as count, MAX(duration) AS slowest')
+                ->where('date', '>=', $now->subHours(match ($this->period) {
+                    '6_hours' => 6,
+                    '24_hours' => 24,
+                    '7_days' => 168,
+                    default => 1,
+                })->toDateTimeString())
+                ->where('duration', '>=', config('pulse.slow_query_threshold'))
+                ->groupBy('route')
+                ->orderByDesc('slowest')
+                ->limit(10)
+                ->get()
+                ->map(function ($row) {
+                    [$method, $path] = explode(' ', $row->route, 2);
+                    $route = Route::getRoutes()->get($method)[$path] ?? null;
+
+                    return [
+                        'uri' => $row->route,
+                        'action' => $route?->getActionName(),
+                        'request_count' => (int) $row->count,
+                        'slowest_duration' => (int) $row->slowest,
+                    ];
+                })
+                ->all();
+
+            $time = (int) ((hrtime(true) - $start) / 1000000);
+
+            return [$slowQueries, $time, $now->toDateTimeString()];
+        });
+
+        $this->dispatchBrowserEvent('slow-queries:dataLoaded');
     }
 }
