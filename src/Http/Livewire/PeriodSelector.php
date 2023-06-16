2baeab859c55ead990cec181328f00ae520bc2e5
Tim MacDonald <hello@timacdonald.me>
2023-06-16 15:02:47 +1000

    Tidy period selector

--- src/Http/Livewire/PeriodSelector.php
+++ src/Http/Livewire/PeriodSelector.php
@@ -3,18 +3,32 @@
 namespace Laravel\Pulse\Http\Livewire;
 
 use Laravel\Pulse\Contracts\ShouldNotReportUsage;
-use Laravel\Pulse\Pulse;
 use Livewire\Component;
 
 class PeriodSelector extends Component implements ShouldNotReportUsage
 {
+    /**
+     * The selected period.
+     *
+     * @var '1_hour'|6_hours'|'24_hours'|'7_days'|null
+     */
     public $period = '1_hour';
 
+    /**
+     * The query string parameters.
+     *
+     * @var array
+     */
     protected $queryString = [
         'period' => ['except' => '1_hour'],
     ];
 
-    public function render(Pulse $pulse)
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render()
     {
         return view('pulse::livewire.period-selector');
     }
