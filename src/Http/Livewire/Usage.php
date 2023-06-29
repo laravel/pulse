0825c222ce8d5e65c59d196b4ac57012c126ac99
timacdonald <timacdonald@users.noreply.github.com>
2023-06-29 06:42:50 +0000

    Fix code styling

--- src/Http/Livewire/Usage.php
+++ src/Http/Livewire/Usage.php
@@ -4,7 +4,6 @@ namespace Laravel\Pulse\Http\Livewire;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Laravel\Pulse\Contracts\ShouldNotReportUsage;
