59843f4a48f20074171e12d698fec6a48ee30e9c
Tim MacDonald <hello@timacdonald.me>
2023-08-17 15:06:45 +1000

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,14 @@
 <x-pulse>
-    {{-- <livewire:servers lazy /> --}}
+    <livewire:servers lazy />
     <livewire:usage lazy />
     {{-- <livewire:usage type="dispatched_job_counts" lazy /> --}}
     {{-- <livewire:usage type="slow_endpoint_counts" lazy /> --}}
     {{-- <livewire:usage type="request_counts" lazy /> --}}
-    {{-- <livewire:exceptions lazy /> --}}
-    {{-- <livewire:slow-routes lazy /> --}}
-    {{-- <livewire:slow-queries lazy /> --}}
-    {{-- <livewire:slow-jobs lazy /> --}}
-    {{-- <livewire:slow-outgoing-requests lazy /> --}}
-    {{-- <livewire:cache lazy /> --}}
-    {{-- <livewire:queues lazy /> --}}
+    <livewire:exceptions lazy />
+    <livewire:slow-routes lazy />
+    <livewire:slow-queries lazy />
+    <livewire:slow-jobs lazy />
+    <livewire:slow-outgoing-requests lazy />
+    <livewire:cache lazy />
+    <livewire:queues lazy />
 </x-pulse>
