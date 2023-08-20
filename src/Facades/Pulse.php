b36fcdc862ea97e96dbe9fdd6840eab60976a863
timacdonald <timacdonald@users.noreply.github.com>
2023-08-20 02:12:17 +0000

    Update facade docblocks

--- src/Facades/Pulse.php
+++ src/Facades/Pulse.php
@@ -5,15 +5,18 @@ namespace Laravel\Pulse\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Laravel\Pulse\Pulse shouldNotRecord()
+ * @method static mixed|null ignore(callable $callback)
+ * @method static \Laravel\Pulse\Pulse stopRecording()
+ * @method static \Laravel\Pulse\Pulse startRecording()
  * @method static \Laravel\Pulse\Pulse filter(callable $filter)
- * @method static \Laravel\Pulse\Pulse record(\Laravel\Pulse\Entries\Entry $entry)
+ * @method static \Laravel\Pulse\Pulse record(\Laravel\Pulse\Entries\Entry|\Laravel\Pulse\Entries\Update $entry)
  * @method static \Laravel\Pulse\Pulse store()
+ * @method static \Illuminate\Support\Collection queue()
  * @method static \Laravel\Pulse\Pulse resolveUsersUsing(callable $callback)
  * @method static \Illuminate\Support\Collection resolveUsers(\Illuminate\Support\Collection $ids)
  * @method static string css()
  * @method static string js()
- * @method static bool check(\Illuminate\Http\Request $request)
+ * @method static bool authorize(\Illuminate\Http\Request $request)
  * @method static \Laravel\Pulse\Pulse auth(callable $callback)
  * @method static \Laravel\Pulse\Pulse ignoreMigrations()
  * @method static bool runsMigrations()
