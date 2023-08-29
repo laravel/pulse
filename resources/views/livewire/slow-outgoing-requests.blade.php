0b623bfca681904eb56d4819a75336b09ce85511
Jess Archer <jess@jessarcher.com>
2023-08-29 16:58:09 +1000

    Update slow outgoing requests icon

--- resources/views/livewire/slow-outgoing-requests.blade.php
+++ resources/views/livewire/slow-outgoing-requests.blade.php
@@ -5,7 +5,7 @@ $supported = method_exists(Illuminate\Http\Client\Factory::class, 'globalMiddlew
     <x-slot:title>
         <x-pulse::card-title class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 mr-2 stroke-gray-500">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
             </svg>
             <span>
                 <span title="Time: {{ $time }}ms; Run at: {{ $runAt }}">Slow Outgoing Requests</span>
