2a686c489438600df9abb51108cfb8a7b79a397b
Tim MacDonald <hello@timacdonald.me>
2023-08-29 17:00:59 +1000

    wip

--- src/Queries/Usage.php
+++ src/Queries/Usage.php
@@ -61,8 +61,8 @@ class Usage
                 'count' => (int) $row->count,
                 'user' => [
                     'name' => $user['name'],
-                    'extra' => $user['extra'] ?? $user['email'],
-                    'avatar' => $user['avatar'] ?? ($user['email'] ?? false) ? "https://unavatar.io/{$user['email']}?fallback=".rawurlencode("https://source.boringavatars.com/marble/120/{$user['email']}?colors=2f2bad,ad2bad,e42692,f71568,f7db15") : null,
+                    'extra' => $user['extra'] ?? $user['email'] ?? '',
+                    'avatar' => $user['avatar'] ?? (($user['email'] ?? false) ? "https://unavatar.io/{$user['email']}?fallback=".rawurlencode("https://source.boringavatars.com/marble/120/{$user['email']}?colors=2f2bad,ad2bad,e42692,f71568,f7db15") : null),
                 ],
             ] : null;
         })
