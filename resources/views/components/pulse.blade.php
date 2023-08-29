bcbc81253927b24c8b0ee4578f1af666dc325e22
Jess Archer <jess@jessarcher.com>
2023-08-29 16:59:01 +1000

    Update title tag

--- resources/views/components/pulse.blade.php
+++ resources/views/components/pulse.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Laravel Pulse</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
