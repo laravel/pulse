c8db68b50ad8adcd8c7e8e67fb20936c9b56667d
Jess Archer <jess@jessarcher.com>
2023-09-01 11:50:11 +1000

    Improve HTTP method badge

--- resources/views/components/http-method-badge.blade.php
+++ resources/views/components/http-method-badge.blade.php
@@ -2,14 +2,11 @@
 
 @php
 $colorClasses = match ($method) {
-    'GET' => 'text-purple-600 bg-purple-200',
-    'OPTIONS' => 'text-purple-600 bg-purple-200',
-    'POST' => 'text-blue-600 bg-blue-200',
-    'PATCH' => 'text-blue-600 bg-blue-200',
-    'PUT' => 'text-blue-600 bg-blue-200',
-    'DELETE' => 'text-red-600 bg-red-200',
-    default => 'text-gray-600 bg-gray-200',
+    'GET', 'OPTIONS' => 'text-purple-600 bg-purple-200 border-purple-300',
+    'POST', 'PUT', 'PATCH' => 'text-blue-600 bg-blue-200 border-blue-300',
+    'DELETE' => 'text-red-600 bg-red-200 border-red-300',
+    default => 'text-gray-600 bg-gray-200 border-gray-300',
 }
 @endphp
 
-<span {{ $attributes->merge(['class' => "text-xs font-mono px-1 rounded font-semibold $colorClasses"]) }}>{{ $method }}</span>
+<span {{ $attributes->merge(['class' => "text-xs font-mono px-1 border rounded font-semibold $colorClasses"]) }}>{{ $method }}</span>
