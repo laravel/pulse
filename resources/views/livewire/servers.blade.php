3b3734bc9b4da7830ccd27779be4f75f9c2e4ee1
Jess Archer <jess@jessarcher.com>
2023-09-02 16:13:33 +1000

    Swap CPU and memory

--- resources/views/livewire/servers.blade.php
+++ resources/views/livewire/servers.blade.php
@@ -29,9 +29,9 @@ $friendlySize = function(int $mb, int $precision = 0) {
         <div class="grid grid-cols-[max-content,minmax(max-content,1fr),max-content,minmax(0,2fr),max-content,minmax(0,2fr),minmax(max-content,1fr)]">
             <div></div>
             <div></div>
-            <div class="text-xs uppercase text-left text-gray-500 font-bold">Memory</div>
-            <div></div>
             <div class="text-xs uppercase text-left text-gray-500 font-bold">CPU</div>
+            <div></div>
+            <div class="text-xs uppercase text-left text-gray-500 font-bold">Memory</div>
             <div></div>
             <div class="text-xs uppercase text-left text-gray-500 font-bold">Storage</div>
             @foreach ($servers as $server)
@@ -51,6 +51,83 @@ $friendlySize = function(int $mb, int $precision = 0) {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 17.25v-.228a4.5 4.5 0 00-.12-1.03l-2.268-9.64a3.375 3.375 0 00-3.285-2.602H7.923a3.375 3.375 0 00-3.285 2.602l-2.268 9.64a4.5 4.5 0 00-.12 1.03v.228m19.5 0a3 3 0 01-3 3H5.25a3 3 0 01-3-3m19.5 0a3 3 0 00-3-3H5.25a3 3 0 00-3 3m16.5 0h.008v.008h-.008v-.008zm-3 0h.008v.008h-.008v-.008z" />
                     </svg>
                     <span class="text-base font-bold text-gray-600">{{ $server->name }}</span>
+                </div>
+                <div class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
+                    <div class="text-xl font-bold text-gray-700 w-14 whitespace-nowrap">
+                        {{ $server->cpu_percent }}%
+                    </div>
+                </div>
+                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
+                    <div
+                        wire:ignore
+                        class="w-full max-w-xs h-9 relative"
+                        x-data="{
+                            init() {
+                                const chart = new Chart(
+                                    this.$refs.canvas,
+                                    {
+                                        type: 'line',
+                                        data: {
+                                            labels: @js(collect($server->readings)->map(fn (stdClass $reading) => $reading->date)),
+                                            datasets: [
+                                                {
+                                                    label: 'CPU Percent',
+                                                    borderColor: '#9333ea',
+                                                    borderWidth: 2,
+                                                    data: @js(collect($server->readings)->map(fn (stdClass $reading) => $reading->cpu_percent ? (int) $reading->cpu_percent : null)),
+                                                    pointStyle: false,
+                                                    tension: 0.2,
+                                                    spanGaps: false,
+                                                },
+                                            ],
+                                        },
+                                        options: {
+                                            maintainAspectRatio: false,
+                                            layout: {
+                                                autoPadding: false,
+                                            },
+                                            scales: {
+                                                x: {
+                                                    display: false,
+                                                    grid: {
+                                                        display: false,
+                                                    },
+                                                },
+                                                y: {
+                                                    display: false,
+                                                    min: 0,
+                                                    max: 100,
+                                                    grid: {
+                                                        display: false,
+                                                    },
+                                                },
+                                            },
+                                            plugins: {
+                                                legend: {
+                                                    display: false,
+                                                },
+                                                tooltip: {
+                                                    callbacks: {
+                                                        title: () => '',
+                                                        label: (context) => `${context.label} - ${context.formattedValue}%`
+                                                    },
+                                                    displayColors: false,
+                                                },
+                                            },
+                                        },
+                                    }
+                                )
+
+                                Livewire.on('chart-update', ({ servers }) => {
+                                    chart.data.labels = servers['{{ $server->slug }}'].readings.map(reading => reading.date)
+                                    chart.data.datasets[0].data = servers['{{ $server->slug }}'].readings.map(reading => reading.cpu_percent)
+                                    chart.update()
+                                })
+                            }
+                        }"
+                    >
+                        <canvas x-ref="canvas" class="w-full ring-1 ring-gray-900/5 bg-white rounded-md shadow-sm"></canvas>
+                    </div>
                 </div>
                 <div class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
                     <div class="w-32 flex-shrink-0 whitespace-nowrap">
@@ -126,83 +203,6 @@ $friendlySize = function(int $mb, int $precision = 0) {
                                 Livewire.on('chart-update', ({ servers }) => {
                                     chart.data.labels = servers['{{ $server->slug }}'].readings.map(reading => reading.date)
                                     chart.data.datasets[0].data = servers['{{ $server->slug }}'].readings.map(reading => reading.memory_used)
-                                    chart.update()
-                                })
-                            }
-                        }"
-                    >
-                        <canvas x-ref="canvas" width="100%" class="ring-1 ring-gray-900/5 bg-white rounded-md shadow-sm"></canvas>
-                    </div>
-                </div>
-                <div class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
-                    <div class="text-xl font-bold text-gray-700 w-14 whitespace-nowrap">
-                        {{ $server->cpu_percent }}%
-                    </div>
-                </div>
-                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
-                    <div
-                        wire:ignore
-                        class="w-full max-w-xs h-9 relative"
-                        x-data="{
-                            init() {
-                                const chart = new Chart(
-                                    this.$refs.canvas,
-                                    {
-                                        type: 'line',
-                                        data: {
-                                            labels: @js(collect($server->readings)->map(fn (stdClass $reading) => $reading->date)),
-                                            datasets: [
-                                                {
-                                                    label: 'CPU Percent',
-                                                    borderColor: '#9333ea',
-                                                    borderWidth: 2,
-                                                    data: @js(collect($server->readings)->map(fn (stdClass $reading) => $reading->cpu_percent ? (int) $reading->cpu_percent : null)),
-                                                    pointStyle: false,
-                                                    tension: 0.2,
-                                                    spanGaps: false,
-                                                },
-                                            ],
-                                        },
-                                        options: {
-                                            maintainAspectRatio: false,
-                                            layout: {
-                                                autoPadding: false,
-                                            },
-                                            scales: {
-                                                x: {
-                                                    display: false,
-                                                    grid: {
-                                                        display: false,
-                                                    },
-                                                },
-                                                y: {
-                                                    display: false,
-                                                    min: 0,
-                                                    max: 100,
-                                                    grid: {
-                                                        display: false,
-                                                    },
-                                                },
-                                            },
-                                            plugins: {
-                                                legend: {
-                                                    display: false,
-                                                },
-                                                tooltip: {
-                                                    callbacks: {
-                                                        title: () => '',
-                                                        label: (context) => `${context.label} - ${context.formattedValue}%`
-                                                    },
-                                                    displayColors: false,
-                                                },
-                                            },
-                                        },
-                                    }
-                                )
-
-                                Livewire.on('chart-update', ({ servers }) => {
-                                    chart.data.labels = servers['{{ $server->slug }}'].readings.map(reading => reading.date)
-                                    chart.data.datasets[0].data = servers['{{ $server->slug }}'].readings.map(reading => reading.cpu_percent)
                                     chart.update()
                                 })
                             }
