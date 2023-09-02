bce3a77aa1c0556a482d123afcdee7a53ad58eeb
Jess Archer <jess@jessarcher.com>
2023-09-02 12:39:04 +1000

    Fix server chart updates when changing period

--- resources/views/livewire/servers.blade.php
+++ resources/views/livewire/servers.blade.php
@@ -9,7 +9,22 @@ $friendlySize = function(int $mb, int $precision = 0) {
     return round($mb, $precision) . 'MB';
 };
 @endphp
-<div wire:poll.5s="updateChart" class="col-span-6">
+<div
+    wire:poll.5s
+    class="col-span-6"
+    x-data="{
+        loadingNewDataset: false,
+        init() {
+            Livewire.on('period-changed', () => (this.loadingNewDataset = true))
+
+            Livewire.hook('commit', ({ component, succeed }) => {
+                if (component.name === 'servers') {
+                    succeed(() => this.loadingNewDataset = false)
+                }
+            })
+        },
+    }"
+>
     @if ($servers->count() > 0)
         <div class="grid grid-cols-[max-content,minmax(max-content,1fr),max-content,minmax(0,2fr),max-content,minmax(0,2fr),minmax(max-content,1fr)]">
             <div></div>
@@ -20,7 +35,7 @@ $friendlySize = function(int $mb, int $precision = 0) {
             <div></div>
             <div class="text-xs uppercase text-left text-gray-500 font-bold">Storage</div>
             @foreach ($servers as $server)
-                <div wire:key="{{ $server->name }}" class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }}" title="{{ $server->updated_at->fromNow() }}">
+                <div wire:key="{{ $server->name }}" class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''" title="{{ $server->updated_at->fromNow() }}">
                     @if ($server->recently_reported)
                         <div class="w-5 flex justify-center mr-1">
                             <div class="h-1 w-1 bg-green-500 rounded-full animate-ping"></div>
@@ -31,13 +46,13 @@ $friendlySize = function(int $mb, int $precision = 0) {
                         </svg>
                     @endif
                 </div>
-                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}">
+                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 mr-2 stroke-gray-500">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 17.25v-.228a4.5 4.5 0 00-.12-1.03l-2.268-9.64a3.375 3.375 0 00-3.285-2.602H7.923a3.375 3.375 0 00-3.285 2.602l-2.268 9.64a4.5 4.5 0 00-.12 1.03v.228m19.5 0a3 3 0 01-3 3H5.25a3 3 0 01-3-3m19.5 0a3 3 0 00-3-3H5.25a3 3 0 00-3 3m16.5 0h.008v.008h-.008v-.008zm-3 0h.008v.008h-.008v-.008z" />
                     </svg>
                     <span class="text-base font-bold text-gray-600">{{ $server->name }}</span>
                 </div>
-                <div class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}">
+                <div class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
                     <div class="w-32 flex-shrink-0 whitespace-nowrap">
                         <span class="text-xl font-bold text-gray-700">
                             {{ $friendlySize($server->memory_used, 1) }}
@@ -47,7 +62,7 @@ $friendlySize = function(int $mb, int $precision = 0) {
                         </span>
                     </div>
                 </div>
-                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}">
+                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
                     <div
                         wire:ignore
                         class="w-full max-w-xs h-9 relative"
@@ -108,7 +123,7 @@ $friendlySize = function(int $mb, int $precision = 0) {
                                     }
                                 )
 
-                                Livewire.on('chartUpdate', ({ servers }) => {
+                                Livewire.on('chart-update', ({ servers }) => {
                                     chart.data.labels = servers['{{ $server->slug }}'].readings.map(reading => reading.date)
                                     chart.data.datasets[0].data = servers['{{ $server->slug }}'].readings.map(reading => reading.memory_used)
                                     chart.update()
@@ -119,12 +134,12 @@ $friendlySize = function(int $mb, int $precision = 0) {
                         <canvas x-ref="canvas" width="100%" class="ring-1 ring-gray-900/5 bg-white rounded-md shadow-sm"></canvas>
                     </div>
                 </div>
-                <div class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}">
+                <div class="flex items-center [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
                     <div class="text-xl font-bold text-gray-700 w-14 whitespace-nowrap">
                         {{ $server->cpu_percent }}%
                     </div>
                 </div>
-                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}">
+                <div class="flex items-center pr-8 xl:pr-12 [&:nth-child(1n+15)]:border-t {{ count($servers) > 1 ? 'py-2' : '' }} {{ ! $server->recently_reported ? 'opacity-25 animate-pulse' : '' }}" :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
                     <div
                         wire:ignore
                         class="w-full max-w-xs h-9 relative"
@@ -185,7 +200,7 @@ $friendlySize = function(int $mb, int $precision = 0) {
                                     }
                                 )
 
-                                Livewire.on('chartUpdate', ({ servers }) => {
+                                Livewire.on('chart-update', ({ servers }) => {
                                     chart.data.labels = servers['{{ $server->slug }}'].readings.map(reading => reading.date)
                                     chart.data.datasets[0].data = servers['{{ $server->slug }}'].readings.map(reading => reading.cpu_percent)
                                     chart.update()
@@ -249,7 +264,7 @@ $friendlySize = function(int $mb, int $precision = 0) {
                                             }
                                         )
 
-                                        Livewire.on('chartUpdate', ({ servers }) => {
+                                        Livewire.on('chart-update', ({ servers }) => {
                                             const storage = servers['{{ $server->slug }}'].storage.find(storage => storage.directory === '{{ $storage->directory }}')
                                             chart.data.datasets[0].data = [
                                                 storage.used,
--- src/Livewire/Servers.php
+++ src/Livewire/Servers.php
@@ -6,24 +6,25 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Support\Facades\View;
 use Laravel\Pulse\Livewire\Concerns\HasPeriod;
 use Laravel\Pulse\Livewire\Concerns\ShouldNotReportUsage;
-use Laravel\Pulse\Queries\Servers as ServersQuery;
 use Livewire\Component;
 
 class Servers extends Component
 {
     use HasPeriod, ShouldNotReportUsage;
 
-    protected $servers;
-
     /**
      * Render the component.
      */
     public function render(callable $query): Renderable
     {
-        // TODO: Chart doesn't update immediately when changing the period.
+        $servers = $query($this->periodAsInterval());
+
+        if (request()->hasHeader('X-Livewire')) {
+            $this->dispatch('chart-update', servers: $servers);
+        }
 
         return View::make('pulse::livewire.servers', [
-            'servers' => $this->servers ??= $query($this->periodAsInterval()),
+            'servers' => $servers,
         ]);
     }
 
@@ -33,15 +34,5 @@ class Servers extends Component
     public function placeholder(): Renderable
     {
         return View::make('pulse::components.placeholder', ['class' => 'col-span-6']);
-    }
-
-    /**
-     * Update the chart.
-     *
-     * @TODO Binding...
-     */
-    public function updateChart(ServersQuery $query)
-    {
-        $this->dispatch('chartUpdate', servers: $this->servers ??= $query($this->periodAsInterval()));
     }
 }
