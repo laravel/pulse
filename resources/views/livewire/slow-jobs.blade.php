49d697ffa6e98da104d4610dcd70ee7a49e0d633
Jess Archer <jess@jessarcher.com>
2023-09-08 14:58:40 +1000

    Remove unnecessary nesting

--- resources/views/livewire/slow-jobs.blade.php
+++ resources/views/livewire/slow-jobs.blade.php
@@ -10,62 +10,62 @@
     </x-pulse::card-header>
 
     <x-pulse::card-body :scroll="$scroll" wire:poll.5s="">
-        <div x-data="{
-            loadingNewDataset: false,
-            init() {
-                Livewire.on('period-changed', () => (this.loadingNewDataset = true))
+        <div
+            x-data="{
+                loadingNewDataset: false,
+                init() {
+                    Livewire.on('period-changed', () => (this.loadingNewDataset = true))
 
-                Livewire.hook('commit', ({ component, succeed }) => {
-                    if (component.name === 'slow-jobs') {
-                        succeed(() => this.loadingNewDataset = false)
-                    }
-                })
-            }
-        }">
-            <div>
-                <div :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''">
-                    @if (count($slowJobs) === 0)
-                        <x-pulse::no-results />
-                    @else
-                        <x-pulse::table>
-                            <colgroup>
-                                <col width="100%" />
-                                <col width="0%" />
-                                <col width="0%" />
-                            </colgroup>
-                            <x-pulse::thead>
-                                <tr>
-                                    <x-pulse::th class="text-left">Job</x-pulse::th>
-                                    <x-pulse::th class="text-right">Count</x-pulse::th>
-                                    <x-pulse::th class="text-right">Slowest</x-pulse::th>
-                                </tr>
-                            </x-pulse::thead>
-                            <tbody>
-                                @foreach ($slowJobs as $job)
-                                    <tr class="h-2 first:h-0"></tr>
-                                    <tr wire:key="{{ $job->job }}">
-                                        <x-pulse::td class="max-w-[1px]">
-                                            <code class="block text-xs text-gray-900 dark:text-gray-100 truncate" title="{{ $job->job }}">
-                                                {{ $job->job }}
-                                            </code>
-                                        </x-pulse::td>
-                                        <x-pulse::td class="text-right text-gray-700 dark:text-gray-300 text-sm w-24 tabular-nums">
-                                            <strong>{{ number_format($job->count) }}</strong>
-                                        </x-pulse::td>
-                                        <x-pulse::td class="text-right text-gray-700 dark:text-gray-300 text-sm w-24 whitespace-nowrap tabular-nums">
-                                            @if ($job->slowest === null)
-                                                <strong>Unknown</strong>
-                                            @else
-                                                <strong>{{ number_format($job->slowest) ?: '<1' }}</strong> ms
-                                            @endif
-                                        </x-pulse::td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </x-pulse::table>
-                    @endif
-                </div>
-            </div>
+                    Livewire.hook('commit', ({ component, succeed }) => {
+                        if (component.name === 'slow-jobs') {
+                            succeed(() => this.loadingNewDataset = false)
+                        }
+                    })
+                }
+            }"
+            class="flex flex-grow"
+            :class="loadingNewDataset ? 'opacity-25 animate-pulse' : ''"
+        >
+            @if (count($slowJobs) === 0)
+                <x-pulse::no-results />
+            @else
+                <x-pulse::table>
+                    <colgroup>
+                        <col width="100%" />
+                        <col width="0%" />
+                        <col width="0%" />
+                    </colgroup>
+                    <x-pulse::thead>
+                        <tr>
+                            <x-pulse::th class="text-left">Job</x-pulse::th>
+                            <x-pulse::th class="text-right">Count</x-pulse::th>
+                            <x-pulse::th class="text-right">Slowest</x-pulse::th>
+                        </tr>
+                    </x-pulse::thead>
+                    <tbody>
+                        @foreach ($slowJobs as $job)
+                            <tr class="h-2 first:h-0"></tr>
+                            <tr wire:key="{{ $job->job }}">
+                                <x-pulse::td class="max-w-[1px]">
+                                    <code class="block text-xs text-gray-900 dark:text-gray-100 truncate" title="{{ $job->job }}">
+                                        {{ $job->job }}
+                                    </code>
+                                </x-pulse::td>
+                                <x-pulse::td class="text-right text-gray-700 dark:text-gray-300 text-sm w-24 tabular-nums">
+                                    <strong>{{ number_format($job->count) }}</strong>
+                                </x-pulse::td>
+                                <x-pulse::td class="text-right text-gray-700 dark:text-gray-300 text-sm w-24 whitespace-nowrap tabular-nums">
+                                    @if ($job->slowest === null)
+                                        <strong>Unknown</strong>
+                                    @else
+                                        <strong>{{ number_format($job->slowest) ?: '<1' }}</strong> ms
+                                    @endif
+                                </x-pulse::td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </x-pulse::table>
+            @endif
         </div>
     </x-pulse::card-body>
 </x-pulse::card>
