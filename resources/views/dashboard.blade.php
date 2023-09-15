482a00cfdb971ba9c3756d0047a4f8c1ca2ceede
Jess Archer <jess@jessarcher.com>
2023-09-15 18:10:02 +1000

    Add first-pass dashboard docs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,41 @@
-<x-pulse>
-    <livewire:pulse.servers />
-    <livewire:pulse.usage />
+{{--
+|--------------------------------------------------------------------------
+| Pulse Dashboard
+|--------------------------------------------------------------------------
+|
+| Here you may configure the cards and layout of the Pulse dashboard.
+|
+| There are 12 columns available by default, but you may customize this by specifying the `cols` attribute on the `<x-pulse>` component as any number between between 1 and 12.
+| You may also make the dashboard full width by setting the `fullWidth` attribute of the `x-pulse` to `true`.
+|
+| Most cards span 6 columns and 1 row on larger screens by default.
+| You may customize this by specifying the `cols` attribute as a number between 1 and 12, and the `rows` attribute between 1 and 6.
+|
+| Most cards also allow setting an `expand` attribute to `true` to show all items, instead of scrolling.
+|
+--}}
+
+<x-pulse :full-width="false" cols="12">
+
+    <livewire:pulse.servers cols="full" />
+
+    <livewire:pulse.usage cols="4" rows="2" />
     {{-- <livewire:pulse.usage type="dispatched_job_counts" /> --}}
     {{-- <livewire:pulse.usage type="slow_endpoint_counts" /> --}}
     {{-- <livewire:pulse.usage type="request_counts" /> --}}
-    <livewire:pulse.exceptions />
-    <livewire:pulse.slow-routes />
-    <livewire:pulse.slow-queries />
-    <livewire:pulse.slow-jobs />
-    <livewire:pulse.slow-outgoing-requests />
-    <livewire:pulse.cache />
-    <livewire:pulse.queues />
+
+    <livewire:pulse.exceptions cols="8" />
+
+    <livewire:pulse.slow-queries cols="8" />
+
+    <livewire:pulse.slow-routes cols="4" />
+
+    <livewire:pulse.slow-jobs cols="4" />
+
+    <livewire:pulse.slow-outgoing-requests cols="4" />
+
+    <livewire:pulse.cache cols="6" />
+
+    <livewire:pulse.queues cols="6" />
+
 </x-pulse>
