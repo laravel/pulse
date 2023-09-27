9fd501a56d9184ac7e7c767030bb6888a76b9311
Jess Archer <jess@jessarcher.com>
2023-09-27 15:29:12 +1000

    Fix 'no results' display

--- resources/views/components/no-results.blade.php
+++ resources/views/components/no-results.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-center justify-center p-4">
+<div class="flex-1 flex flex-col items-center justify-center p-4">
     <x-pulse::icons.clipboard class="h-6 w-6 stroke-gray-300" />
     <p class="mt-2 text-sm text-gray-400">
         No results
