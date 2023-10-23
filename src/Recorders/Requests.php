a7920275ce94ad8ec0f8a7c86bfba1ac17ca9a77
Tim MacDonald <hello@timacdonald.me>
2023-10-23 12:19:30 +1100

    Fix config

--- src/Recorders/Requests.php
+++ src/Recorders/Requests.php
@@ -66,7 +66,7 @@ class Requests
             'route' => $request->method().' '.$path,
             'duration' => $duration = $startedAt->diffInMilliseconds(),
             'user_id' => $this->pulse->authenticatedUserIdResolver(),
-            'slow' => $duration >= $this->config->get('pulse.recorders.'.self::class.'.threshold'),
+            'slow' => $duration >= $this->config->get('pulse.recorders.'.static::class.'.threshold'),
         ]);
     }
 
