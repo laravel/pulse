177f63aef0dacd026cbefb0d1c8ccd308718aae5
Jess Archer <jess@jessarcher.com>
2023-11-05 09:30:53 +1000

    Update usage and exceptions cards instantly when changing the dropdown

--- resources/views/livewire/exceptions.blade.php
+++ resources/views/livewire/exceptions.blade.php
@@ -11,7 +11,7 @@
             <div class="flex border border-gray-200 dark:border-gray-700 overflow-hidden rounded-md focus-within:ring">
                 <label class="px-3 flex items-center border-r border-gray-200 dark:border-gray-700 text-xs sm:text-sm text-gray-600 dark:text-gray-300 whitespace-nowrap bg-gray-100 dark:bg-gray-800/50">Sort by</label>
                 <select
-                    wire:model="orderBy"
+                    wire:model.live="orderBy"
                     wire:change="$dispatch('exception-changed', { orderBy: $event.target.value })"
                     class="overflow-ellipsis w-full border-0 pl-3 pr-8 py-1 bg-gray-50 dark:bg-gray-800 text-gray-700 dark:text-gray-300 text-xs sm:text-sm shadow-none focus:ring-0"
                 >
--- resources/views/livewire/usage.blade.php
+++ resources/views/livewire/usage.blade.php
@@ -22,8 +22,7 @@
                 <div class="flex border border-gray-200 dark:border-gray-700 overflow-hidden rounded-md focus-within:ring">
                     <label class="px-3 flex items-center border-r border-gray-200 dark:border-gray-700 text-xs sm:text-sm text-gray-600 dark:text-gray-300 whitespace-nowrap bg-gray-100 dark:bg-gray-800/50">Top 10 users</label>
                     <select
-                        x-ref="select"
-                        wire:model="usage"
+                        wire:model.live="usage"
                         wire:change="$dispatch('usage-changed', { usage: $event.target.value })"
                         class="overflow-ellipsis w-full border-0 pl-3 pr-8 py-1 bg-gray-50 dark:bg-gray-800 text-gray-700 dark:text-gray-300 text-xs sm:text-sm shadow-none focus:ring-0"
                     >
