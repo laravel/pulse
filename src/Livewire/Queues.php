1446f43fffafce9353dea36129b393f456d011cb
Tim MacDonald <hello@timacdonald.me>
2023-12-01 21:16:24 +1100

    sort queues

--- src/Livewire/Queues.php
+++ src/Livewire/Queues.php
@@ -30,6 +30,8 @@ class Queues extends Card
             $this->periodAsInterval(),
         ));
 
+        $queues = $queues->sortKeys();
+
         if (Request::hasHeader('X-Livewire')) {
             $this->dispatch('queues-chart-update', queues: $queues);
         }
