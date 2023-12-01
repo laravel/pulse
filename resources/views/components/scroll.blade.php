4853fa42b6f63ffd9df5b2d21466ef14a2b2435a
AJ <alvaro.meireles@live.com>
2023-12-01 19:45:54 -0300

    introducing soft scrollbar

--- resources/views/components/scroll.blade.php
+++ resources/views/components/scroll.blade.php
@@ -22,7 +22,7 @@
     }"
     {{ $attributes->merge(['class' => '@container/scroll-wrapper flex-grow flex w-full overflow-hidden' . ($expand ? '' : ' basis-56'), ':class' => "loading && 'opacity-25 animate-pulse'"]) }}
 >
-    <div x-ref="content" class="flex-grow basis-full overflow-y-auto" @scroll.debounce.5ms="scroll">
+    <div x-ref="content" class="flex-grow basis-full overflow-y-auto soft-scrollbar" @scroll.debounce.5ms="scroll">
         {{ $slot }}
         <div x-ref="fade" class="h-6 origin-bottom fixed bottom-0 left-0 right-0 bg-gradient-to-t from-white dark:from-gray-900 pointer-events-none" wire:ignore></div>
     </div>
