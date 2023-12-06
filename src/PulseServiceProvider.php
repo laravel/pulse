df22f3c8ac908f9414256c4ef27e8628ea713daa
Xu Chunyang <xuchunyang56@gmail.com>
2023-12-06 11:19:09 +0800

    Show pulse version from artisan about command

--- src/PulseServiceProvider.php
+++ src/PulseServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace Laravel\Pulse;
 
+use Composer\InstalledVersions;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Http\Kernel as HttpKernel;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Queue\Events\Looping;
 use Illuminate\Queue\Events\WorkerStopping;
 use Illuminate\Routing\Router;
@@ -76,6 +78,10 @@ class PulseServiceProvider extends ServiceProvider
         $this->registerResources();
         $this->registerPublishing();
         $this->registerCommands();
+
+        AboutCommand::add('Pulse', fn () => [
+            'Version' => InstalledVersions::getPrettyVersion('laravel/pulse'),
+        ]);
     }
 
     /**
