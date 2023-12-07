68717a2cbf9f90f74d347b776704c43952449799
Tim MacDonald <hello@timacdonald.me>
2023-12-07 13:34:30 +1100

    Add the ability to disable SQL highlighting

--- config/pulse.php
+++ config/pulse.php
@@ -194,6 +194,7 @@ return [
             'sample_rate' => env('PULSE_SLOW_QUERIES_SAMPLE_RATE', 1),
             'threshold' => env('PULSE_SLOW_QUERIES_THRESHOLD', 1000),
             'location' => env('PULSE_SLOW_QUERIES_LOCATION', true),
+            'highlighting' => env('PULSE_SLOW_QUERIES_HIGHLIGHTING', true),
             'ignore' => [
                 '/(["`])pulse_[\w]+?\1/', // Pulse tables...
             ],
