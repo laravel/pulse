503cb7c582926afd66dd3425150beab0cd59ffd9
timacdonald <timacdonald@users.noreply.github.com>
2023-12-07 01:19:49 +0000

    Fix code styling

--- src/Support/RedisServerException.php
+++ src/Support/RedisServerException.php
@@ -13,7 +13,7 @@ class RedisServerException extends RuntimeException
     /**
      * Create an exception from the client's error message.
      */
-    public static function whileRunningCommand(string $command, string $message, Throwable $previous = null): self
+    public static function whileRunningCommand(string $command, string $message, ?Throwable $previous = null): self
     {
         if (str_starts_with($message, 'ERR syntax error')) {
             $message = "The Redis version does not support the command or some of its arguments [{$command}]. Redis error: [{$message}].";
