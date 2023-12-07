ab7c35917ed2b51eaa5283d38fe814975d209c72
Matt <matt@mralston.co.uk>
2023-12-07 11:52:30 +0000

    Update 2023_06_07_000001_create_pulse_tables.php
    Fixed typo.

--- database/migrations/2023_06_07_000001_create_pulse_tables.php
+++ database/migrations/2023_06_07_000001_create_pulse_tables.php
@@ -31,7 +31,7 @@ return new class extends Migration
             match ($driver = $connection->getDriverName()) {
                 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
                 'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
-                'sqlite' => App::environment('testing') ? $table->char('key_hash', 16) : throw new RuntimeException("Unsupported database driver [{$driver}].")
+                'sqlite' => App::environment('testing') ? $table->char('key_hash', 16) : throw new RuntimeException("Unsupported database driver [{$driver}]."),
                 default => throw new RuntimeException("Unsupported database driver [{$driver}]."),
             };
             $table->text('value');
@@ -49,7 +49,7 @@ return new class extends Migration
             match ($driver = $connection->getDriverName()) {
                 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
                 'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
-                'sqlite' => App::environment('testing') ? $table->char('key_hash', 16) : throw new RuntimeException("Unsupported database driver [{$driver}].")
+                'sqlite' => App::environment('testing') ? $table->char('key_hash', 16) : throw new RuntimeException("Unsupported database driver [{$driver}]."),
                 default => throw new RuntimeException("Unsupported database driver [{$driver}]."),
             };
             $table->bigInteger('value')->nullable();
@@ -69,7 +69,7 @@ return new class extends Migration
             match ($driver = $connection->getDriverName()) {
                 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
                 'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
-                'sqlite' => App::environment('testing') ? $table->char('key_hash', 16) : throw new RuntimeException("Unsupported database driver [{$driver}].")
+                'sqlite' => App::environment('testing') ? $table->char('key_hash', 16) : throw new RuntimeException("Unsupported database driver [{$driver}]."),
                 default => throw new RuntimeException("Unsupported database driver [{$driver}]."),
             };
             $table->string('aggregate');
