3e540495fe66616f91625888fe643c5a57adf08e
Tim MacDonald <hello@timacdonald.me>
2023-12-11 12:57:14 +1100

    Static analysis

--- src/Livewire/Usage.php
+++ src/Livewire/Usage.php
@@ -50,7 +50,7 @@ class Usage extends Card
                         'slow_requests' => 'slow_user_request',
                         'jobs' => 'user_job',
                     },
-                    'count', // @phpstan-ignore argument.type
+                    'count',
                     $this->periodAsInterval(),
                     limit: 10,
                 );
