9a2594008a0ff501e6a5d41e654ec5ac7941078c
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2023-12-14 09:38:55 +0100

    fix: use PULSE_PATH env instead of fixed text for /pulse

--- config/pulse.php
+++ config/pulse.php
@@ -204,7 +204,7 @@ return [
             'sample_rate' => env('PULSE_SLOW_REQUESTS_SAMPLE_RATE', 1),
             'threshold' => env('PULSE_SLOW_REQUESTS_THRESHOLD', 1000),
             'ignore' => [
-                '#^/pulse$#', // Pulse dashboard...
+                '#^/' . env('PULSE_PATH', 'pulse') . '$#', // Pulse dashboard...
             ],
         ],
 
@@ -220,7 +220,7 @@ return [
             'enabled' => env('PULSE_USER_REQUESTS_ENABLED', true),
             'sample_rate' => env('PULSE_USER_REQUESTS_SAMPLE_RATE', 1),
             'ignore' => [
-                '#^/pulse$#', // Pulse dashboard...
+                '#^/' . env('PULSE_PATH', 'pulse') . '$#', // Pulse dashboard...
             ],
         ],
     ],
