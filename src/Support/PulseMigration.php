7cf1593ef6b26df0745b85e7e05229ee7b61519c
Tim MacDonald <hello@timacdonald.me>
2023-12-15 10:30:59 +1100

    [1.x] Improve migration errors to guide developers with unsupported drivers (#233)
    * Improve migration errors to guide developers with unsupported drivers
    
    * Fix code styling
    
    * Fix
    
    * Update PulseMigration.php
    
    ---------
    
    Co-authored-by: timacdonald <timacdonald@users.noreply.github.com>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

---
+++ src/Support/PulseMigration.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Laravel\Pulse\Support;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use RuntimeException;
+
+class PulseMigration extends Migration
+{
+    /**
+     * Get the migration connection name.
+     */
+    public function getConnection(): ?string
+    {
+        return Config::get('pulse.storage.database.connection');
+    }
+
+    /**
+     * Determine if the migration should run.
+     */
+    protected function shouldRun(): bool
+    {
+        if (in_array($this->driver(), ['mysql', 'pgsql'])) {
+            return true;
+        }
+
+        if (! App::environment('testing')) {
+            throw new RuntimeException("Pulse does not support the [{$this->driver()}] database driver.");
+        }
+
+        if (Config::get('pulse.enabled')) {
+            throw new RuntimeException("Pulse does not support the [{$this->driver()}] database driver. You can disable Pulse in your testsuite by adding `<env name=\"PULSE_ENABLED\" value=\"false\"/>` to your project's `phpunit.xml` file.");
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the database connection driver.
+     */
+    protected function driver(): string
+    {
+        return DB::connection($this->getConnection())->getDriverName();
+    }
+}
