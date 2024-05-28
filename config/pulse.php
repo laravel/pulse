f0c59b7d319d9f723d894e254365c2363e41e673
ziming <ziming@users.noreply.github.com>
2024-05-29 00:48:19 +0800

    allow pulse title to be changed

--- config/pulse.php
+++ config/pulse.php
@@ -8,6 +8,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Pulse App Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to display the name of the application within the UI
+    | or in other locations. Of course, you're free to change the value.
+    |
+    */
+    'name' => env('PULSE_APP_NAME', '<b class="font-bold">Laravel</b> Pulse'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Pulse Domain
     |--------------------------------------------------------------------------
     |
