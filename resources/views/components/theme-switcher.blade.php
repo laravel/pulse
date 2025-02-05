1f2ca0648ca88d5cd48e6e9ae158fc14e02e835f
J-T-McC <tyson_mcc@hotmail.com>
2025-02-05 13:20:15 -0800

    Default system if theme unrecognized

--- resources/views/components/theme-switcher.blade.php
+++ resources/views/components/theme-switcher.blade.php
@@ -1,6 +1,6 @@
 <script>
 setDarkClass = () => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (localStorage.theme === 'dark' || ((!('theme' in localStorage) || !['light', 'dark'].includes(localStorage.theme)) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark')
     } else {
         document.documentElement.classList.remove('dark')
