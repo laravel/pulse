5638c8bb23a39960c4fcaf132806c2075a28705c
error1001es <49606492+error1001es@users.noreply.github.com>
2026-03-01 00:59:08 +0300

    fix: Handle unsupported drivers in Pulse migrations

--- database/migrations/2023_06_07_000001_create_pulse_tables.php
+++ database/migrations/2023_06_07_000001_create_pulse_tables.php
@@ -24,6 +24,7 @@ return new class extends PulseMigration
                 'mariadb', 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
                 'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
                 'sqlite' => $table->string('key_hash'),
+                default => throw new \RuntimeException('Unsupported database driver: ' . $this->driver()),
             };
             $table->mediumText('value');
 
@@ -41,6 +42,7 @@ return new class extends PulseMigration
                 'mariadb', 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
                 'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
                 'sqlite' => $table->string('key_hash'),
+                default => throw new \RuntimeException('Unsupported database driver: ' . $this->driver()),
             };
             $table->bigInteger('value')->nullable();
 
@@ -60,6 +62,7 @@ return new class extends PulseMigration
                 'mariadb', 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
                 'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
                 'sqlite' => $table->string('key_hash'),
+                default => throw new \RuntimeException('Unsupported database driver: ' . $this->driver()),
             };
             $table->string('aggregate');
             $table->decimal('value', 20, 2);
