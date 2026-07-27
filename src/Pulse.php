cbe43dd9b684239717f96491b6e6d61dc23c5e3a
Mior Muhammad Zaki <crynobone@gmail.com>
2026-07-27 15:44:07 +0800

    [1.x] Use PHPStan 2
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Pulse.php
+++ src/Pulse.php
@@ -121,16 +121,17 @@ class Pulse
         $this->afterResolving($this->app, 'events', fn (Dispatcher $event) => $recorders
             ->filter(fn ($recorder) => $recorder->listen ?? null)
             ->each(fn ($recorder) => $event->listen(
-                $recorder->listen,
-                fn ($event) => $this->rescue(fn () => $recorder->record($event))
+                $recorder->listen, // @phpstan-ignore property.notFound
+                fn ($event) => $this->rescue(fn () => $recorder->record($event)) // @phpstan-ignore method.notFound,argument.templateType
             ))
         );
 
         $recorders
             ->filter(fn ($recorder) => method_exists($recorder, 'register'))
             ->each(function ($recorder) {
+                /* @phpstan-ignore method.notFound */
                 $this->app->call($recorder->register(...), [
-                    'record' => fn (...$args) => $this->rescue(fn () => $recorder->record(...$args)),
+                    'record' => fn (...$args) => $this->rescue(fn () => $recorder->record(...$args)), // @phpstan-ignore method.notFound,argument.templateType
                 ]);
             });
 
@@ -303,7 +304,7 @@ class Pulse
             $ingest = $this->app->make(Ingest::class);
 
             $count = $this->rescue(function () use ($entries, $ingest) {
-                $ingest->ingest($entries); // @phpstan-ignore argument.type
+                $ingest->ingest($entries); /* @phpstan-ignore argument.type */
 
                 return $entries->count();
             }) ?? 0;
